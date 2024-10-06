0bd7ae08ac1ba327bab3655164def7da9b31d6ab
kashishksy <kashishksy2112@gmail.com>
2024-10-06 11:46:58 -0400

    adding necessary comments and delete request

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import data  from "./data.js";
+import products  from "./data.js";
 import express from "express";
 
 const app = express();
@@ -8,29 +8,55 @@ const portNumber = 3000;
 app.use(express.json());
 
 app.listen(portNumber, ()=> {
+
+    //logging endpoint info onto the console
     console.log(`Listening on port ${portNumber}`)
-    console.log(`Endpoints: GET request: 'http://localhost:3000/allData' `)
+    console.log(`Endpoints: GET request: 'http://localhost:3000/products' `)
     console.log(`POST request: 'http://localhost:3000/postRequest' `)
 })
 
+//initiating variables for request counters
 let getRequestCount = 0
 let postRequestCount = 0
 
-app.get("/allData", (req,res)=> {
+//get request 
+app.get("/products", (req,res)=> {
+
+    //logging on console for get request
     console.log("Recieved GET request...")
     getRequestCount += 1
     console.log(`Processed request count --> GET: ${getRequestCount} POST: ${postRequestCount}`)
-    res.send(data)
+    res.send(products)
 })
 
 
+//post request
 app.post("/postRequest",(req,res) => {
 
     let postData = req.body
+
+    //logging on console for post request
     console.log(`Received POST request: ${JSON.stringify(postData, null, 2)}, sending response...`)
     postRequestCount += 1
+
+    //adding data to my data.js array
+    products.push(postData);  
     console.log(`Processed request count --> GET: ${getRequestCount} POST: ${postRequestCount}`)
     res.send(postData)
     
+   
+})
+
+//delete request
+app.delete("/deleteData",(req,res) => {
+
+    //logging on console for delete request
+    console.log("Received DELETE request, deleting all data...");
+
+    //clear the array/ data object
+    products.length = 0
+
+    //finally, send response
+    res.send({ message: "All data deleted." });
    
 })
